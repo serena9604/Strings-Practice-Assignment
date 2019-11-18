ba63c6267b6fd5015a891ff427b8154ee20748b9
Serena Bi <56049445+serena9604@users.noreply.github.com>
2019-11-17 20:32:37 -0800

    Add files via upload

---
+++ Unit 3 Strings Assignment/PPAP.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class PPAP
+{
+    public static void nextLine (String prevLine) {
+
+    }
+
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        String prevLine = sc.next();
+        nextLine(prevLine);
+
+    }
+}
